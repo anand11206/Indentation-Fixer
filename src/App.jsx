72262bcd0a0826733f0433c11c473a120e9a06bb
anand11206 <anand110206@gmail.com>
2025-06-21 12:51:08 +0530

    Add Monaco Editor integration and update code handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useCallback, useRef, useState } from 'react';
 import * as parser from '@babel/parser';
 import * as generator from '@babel/generator';
-
+import Editor from "@monaco-editor/react"
 const generate = generator.default;
 
+ 
 function indent(code, lang, tabsize) {
   if (lang === "js") {
     let ast = parser.parse(code);
@@ -13,20 +14,45 @@ function indent(code, lang, tabsize) {
   return 0;
 }
 function App() {
+  
   const [code, setCode] = useState(`function foo( ){console.log("bad")}`);
   const [lang, setLang] = useState("js");
   const codeRef = useRef(null);
   const copy = useCallback(() => {
-    codeRef.current?.select();
+    const model = codeRef.current.getModel();
+    const fullRange = model.getFullModelRange(); // Gets full range of text
+
+    codeRef.current.setSelection(fullRange); // Highlights the entire code
+    codeRef.current.focus(); // Focus the editor to see the selection
     window.navigator.clipboard.writeText(code);
   }, [code]);
   const fixIndentation = useCallback(() => {
     setCode(indent(code, lang, 4));
   }, [code, lang]);
-  return <div className=' flex flex-col items-center  bg-blue-500 h-screen w-full p-5'>
-        		<div className='text-6xl mb-5 mt-3 p-7 border-2 rounded-2xl bg-white'>Code Indentation Fixer</div>
-        		<div className='flex flex-col items-center justify-center rounded-2xl p-10 h-full w-200 bg-white'>
-        			<textarea className="border-2 border-black rounded-md w-180 h-90 resize p-2" value={code} ref={codeRef} onChange={e => setCode(e.target.value)} />
+  const  handleEditorDidMount = useCallback((editor, monaco)=> {
+    // here is the editor instance
+    // you can store it in `useRef` for further usage
+    codeRef.current = editor;
+    console.log(codeRef)
+  },[codeRef])
+  return (
+    <>
+    <div className='border-1 h-20 flex items-center justify-center'>
+      <h1 className='text-4xl'>Code Fixer</h1>
+    </div>
+    <div className='flex flex-col items-center justify-center' >
+      <div className='w-340 m-8'>
+        <Editor 
+        className="border-1 border-black " 
+        height="60vh"
+        value={code}
+        onMount={handleEditorDidMount}
+        onChange={(e)=> setCode(e.target.value)}
+        ref={codeRef}
+        />
+
+      </div>
+      <div className='flex flex-col items-center justify-center'> 
         <select name="" id="" value={lang} onChange={e => setLang(e.target.value)}>
         <option value="py">Python</option>
         <option value="js">JavaScript</option>
@@ -39,8 +65,10 @@ function App() {
         </button>
         <button className="border-1 border-blue-700 rounded-lg p-0.5 pr-2 pl-2 cursor-pointer bg-blue-700 text-white h-12 w-25 m-2" onClick={copy}>Copy</button>
         </div>
-        		</div>
-        	
-        	</div>;
+      </div>
+     
+    </div>
+    </>
+  )
 }
 export default App;
